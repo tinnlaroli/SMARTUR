a51e3a70a9dd0296ee984d83c636739f8093d672
tinnlaroli <martinlaraolivares@gmailcom>
2026-06-26 23:25:49 -0600

    feat: sw cache & errores welltur

--- VIDEO/src/Root.tsx
+++ VIDEO/src/Root.tsx
@@ -1,17 +1,28 @@
 import { Composition, registerRoot } from "remotion";
 import { SmartURPromo } from "./SmartURPromo";
+import { PrestadorPromo, PRESTADOR_TOTAL_FRAMES } from "./scenes/PrestadorPromo";
 import { TOTAL_FRAMES } from "./helpers/timing";
 
 function RemotionRoot() {
   return (
-    <Composition
-      id="SmartURPromo"
-      component={SmartURPromo}
-      durationInFrames={TOTAL_FRAMES}
-      fps={30}
-      width={1080}
-      height={1920}
-    />
+    <>
+      <Composition
+        id="SmartURPromo"
+        component={SmartURPromo}
+        durationInFrames={TOTAL_FRAMES}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+      <Composition
+        id="PrestadorPromo"
+        component={PrestadorPromo}
+        durationInFrames={PRESTADOR_TOTAL_FRAMES}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+    </>
   );
 }
 
